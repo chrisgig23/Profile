4131e670d8ca4fc536926397696b226a3f60b448
Christopher Giglio <cgiglio23@gmail.com>
2019-01-31 19:50:11 -0500

    Added playable TicTacToe game

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def welcomeHome():
     return render_template('index.html')
 
+@app.route('/tictactoe')
+def playGame():
+    return render_template('game.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
