045bb4f31db1be2def2023464095013090500b7e
Christopher Giglio <cgiglio23@gmail.com>
2019-02-01 19:01:51 -0500

    Added Nav Bar to Profile page

---
+++ static/js/navBar.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("topNav");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
