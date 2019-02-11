cb6cda03a295a8dba57d39052f759af61c4acbca
Christopher Giglio <cgiglio23@gmail.com>
2019-02-11 09:28:29 -0500

    Implemented sending mail functionality for contact page

--- __init__.py
+++ __init__.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request, redirect
 from flask import url_for, flash, jsonify
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'chris.giglio.com@gmail.com'
+app.config['MAIL_PASSWORD'] = 'sPunky23!'
+app.config['MAIL_DEFAULT_SENDER'] = 'chris.giglio.com@gmail.com'
+app.config['MAIL_MAX_EMAILS'] = None
+# app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+mail = Mail(app)
 
 
 @app.route('/')
@@ -15,6 +31,13 @@ def playGame():
 @app.route('/contact')
 def contactMe():
     return render_template('contact.html')
+
+@app.route('/testing')
+def testingMail():
+    msg = Message('Testing', recipients = ['cgiglio23@gmail.com'])
+    mail.send(msg)
+
+    return 'success'
 
 if __name__ == '__main__':
     # app.debug = True
