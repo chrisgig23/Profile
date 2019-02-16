a6b56c3ca7ccf8fb9e9d1725cbedbe342099c78a
Christopher Giglio <cgiglio23@gmail.com>
2019-02-16 09:19:17 -0500

    Animate mobile menu icon complete

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -28,5 +28,6 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
+  showHideMenu();
   // Do something else, like open/close menu
 });
